7271ed120b72977fbd0cc81eb1c9fa1b7dd1d113
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-16 14:41:24 +0900

    ✏️ Feat :  prettier eslint 설정

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
 const port = 5000;
 
 console.log(port);
+
+// app.listen(PORT, () => {
+//   console.log(`
+//   ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+//   ┃   Server listening on port: ${PORT}    ┃
+//   ┃     http://localhost:${PORT}/api       ┃
+//   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+//   `);
+// });
