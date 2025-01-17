c2ff176fd869da2c0e9b8af8c925254822391cfd
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-17 15:53:10 +0900

    📦️ Chore : install & setting CORS

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,14 @@ import { DEV_LOG } from "@config";
 
 import routes from "@routes";
 
+import cors from "cors";
 import morgan from "morgan";
 
 const app = express();
 
 app.use(morgan(DEV_LOG || "combined"));
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
