7a007590a643f65474f810567db9103bf4dfff34
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-18 16:02:18 +0900

    [Refactoring ⚙️] Post Service 구조 변경

--- src/services/post/post.service.ts
+++ src/services/post/post.service.ts
@@ -1,48 +1,4 @@
-import { fireStore } from "@config";
-
-import { Post } from "@interfaces";
-
-import {
-  collection,
-  getDocs,
-  limit,
-  orderBy,
-  query,
-  startAfter,
-} from "firebase/firestore";
-
-const getAllPosts = async ({ cursor }: { cursor?: string }) => {
-  let queryRef;
-
-  if (cursor) {
-    queryRef = query(
-      collection(fireStore, "posts"),
-      orderBy("createAt", "desc"),
-      startAfter(cursor),
-      limit(4),
-    );
-  } else {
-    queryRef = query(
-      collection(fireStore, "posts"),
-      orderBy("createAt", "desc"),
-      limit(4),
-    );
-  }
-
-  const res = await getDocs(queryRef).catch(() => {
-    throw Error("Failed to fetch posts list all");
-  });
-
-  const result: Post[] = [];
-
-  res.forEach(doc => {
-    if (doc.exists()) {
-      result.push(doc.data() as Post);
-    }
-  });
-
-  return result;
-};
+import getAllPosts from "./apis/getAllPosts";
 
 const PostService = {
   getAllPosts,
