c43097a6999afa968bd5eff2161f99d0d8f8385c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-24 16:45:14 +0900

    ✏️ Feat :  getOptions Router 연결

--- src/routes/options/options.router.ts
+++ src/routes/options/options.router.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 
+import { OptionsController } from "@controllers";
+
 import { Routes } from "@interfaces";
 
 const route = Router();
+
+route.get("/get/:type", OptionsController.getOptions);
 
 const optionsRouter: Routes = {
   path: "/options",
