715a96cc75a3e9d7ca45a3fb6f2b87231872451a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-04-27 22:58:03 +0900

    👷 Build :  getPostList

--- dist/routes/post/post.router.js
+++ dist/routes/post/post.router.js
@@ -4,6 +4,7 @@ const express_1 = require("express");
 const _controllers_1 = require("../../controllers/index.js");
 const route = (0, express_1.Router)();
 route.get("/all", _controllers_1.PostController.getAllPosts);
+route.get("/list", _controllers_1.PostController.getPostList);
 const postRouter = {
     path: "/post",
     router: route,
