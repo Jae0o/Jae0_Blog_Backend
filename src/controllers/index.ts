945f8c61c0d04aa44962c6c53fa38b1bee098274
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-05-09 20:47:16 +0900

    ♻️ Refactor :  getOptions 분리 및 모듈 구조 변경

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
 export { default as PostController } from "./post";
 export * from "./post/index.type";
 
-export { default as OptionsController } from "./options/options.controller";
+export { default as OptionsController } from "./options";
+export * from "./options/index.type";
