4c3c719aeb17a6b15919cf3f138595a1bfea30cd
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-05-09 19:45:24 +0900

    ✏️ Feat :  getAllPosts Validation

--- src/validations/post/index.ts
+++ src/validations/post/index.ts
@@ -1,9 +1,11 @@
+import getAllPostsValidation from "./getAllPosts/getAllPosts.validation";
 import getPostValidation from "./getPost/getPost.validation";
 import getPostListValidation from "./getPostList/getPostList.validation";
 
 const PostValidation = {
   getPost: getPostValidation,
   getPostList: getPostListValidation,
+  getAllPosts: getAllPostsValidation,
 };
 
 export default PostValidation;
