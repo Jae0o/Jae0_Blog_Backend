b67905c264c05c8efaf82f645fd38ab89a806185
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-05-09 19:26:16 +0900

    ✏️ Feat :  StatusCode Server error 추가 및 미들웨어 적용

--- src/middlewares/ErrorMiddleware/error.middleware.ts
+++ src/middlewares/ErrorMiddleware/error.middleware.ts
@@ -1,8 +1,10 @@
 import { ErrorRequestHandler } from "express";
 
+import { BlogStatusCode } from "@interfaces";
+
 const errorMiddleware: ErrorRequestHandler = (error, req, res, next) => {
   try {
-    const status: number = error.status || 500;
+    const status: number = error.status || BlogStatusCode.SERVER_ERROR;
     const message = error.message || "unknown error";
 
     res.status(status).json({ message });
