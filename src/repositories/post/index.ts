d4d5aea24835815e84ad7813c775c47d7624a9ee
Jae0 <leey153@naver.com>
2026-06-19 22:42:05 +0900

    ✏️ Feat : utils text · post countPublic 바렐 export 누락 보강 (M1)

--- src/repositories/post/index.ts
+++ src/repositories/post/index.ts
@@ -1,7 +1,8 @@
+import { countPublic } from './countPublic';
 import { getAdjacent } from './getAdjacent';
 import { getByIds } from './getByIds';
 import { getBySlug } from './getBySlug';
 import { listPublic } from './listPublic';
 
 /** posts Firestore 접근 (공개 read). */
-export const PostRepository = { listPublic, getBySlug, getByIds, getAdjacent };
+export const PostRepository = { listPublic, countPublic, getBySlug, getByIds, getAdjacent };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './AppError';
 export * from './asyncHandler';
 export * from './ip';
 export * from './pagination';
+export * from './text';
